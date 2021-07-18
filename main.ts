af8990575d2cfb1b199ab2a8a069323596260c64
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2021-07-18 11:39:47 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
-addEventListener("fetch", (event) => {
-  const response = new Response("my new deno is success 2 !", {
-    headers: { "content-type": "text/plain" },
-  });
-  event.respondWith(response);
+import { Application } from "https://deno.land/x/oak/mod.ts";
+
+const app = new Application();
+
+app.use((ctx) => {
+  ctx.response.body = {
+    data:"your function is ready to use "
+  }
+  
 });
+
+
+addEventListener("fetch",app.fetchEventHandler());
