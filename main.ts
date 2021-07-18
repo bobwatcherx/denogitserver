bbb7985b6cf20b066ef05eabe8bc3d6a06973734
bobwatcherx <52072811+bobwatcherx@users.noreply.github.com>
2021-07-18 11:37:42 +0700

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("my new deno is success 2 !", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
